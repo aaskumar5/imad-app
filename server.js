8880280d13d77be39503176cb5a754b7e7112581
Ashish Kumar sahu <32214433+aaskumar5@users.noreply.github.com>
2018-02-15 11:33:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get('/article-one',function (req,res){
 app.get('/article-two',function (req,res){
     res.send('Article two requested and will sereve here');
 });
+app.get('/article-three',function (req,res){
+    res.send('Article three requested and will sereve here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
