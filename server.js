7fcc4d67237e4f31bd14366bbd38f60d2887d612
Ashish Kumar sahu <32214433+aaskumar5@users.noreply.github.com>
2018-02-16 21:01:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/article-three',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 var counter=0;
-app.get('/counter',function (req,res){
+app.get('/c',function (req,res){
     counter = counter + 1;
    res.send(counter.toString());
 });
