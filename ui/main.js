4f82f82b6ed07db6970e5a00947510f35b36db2a
Ashish Kumar sahu <32214433+aaskumar5@users.noreply.github.com>
2018-02-16 21:36:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,7 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML = 'new value';
-
-//move image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+var button = document.getElementById('counter');
+button.onclick = function() {
+    
+    counter = counter + 1
+    var span = document.getElementById('count');
+    span.innnerHTML = counter.toString();
 }
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-};
