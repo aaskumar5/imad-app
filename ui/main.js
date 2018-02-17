cd89755ba02c06fb4e712dfcc6e1621fa570d1e8
Ashish Kumar sahu <32214433+aaskumar5@users.noreply.github.com>
2018-02-17 13:00:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,7 @@ button.onclick = function() {
    
 };
 
-var nameInput= document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -34,4 +33,10 @@ submit.onclick = function(){
     }
     var ul = document.getElementById('namelist');
     ul.innnerHTML = list;
+    
+    var nameInput= document.getElementById('name');
+var name = nameInput.value;
+
+     request.open('GET','http://ashishkumarsahu2016.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
 };
