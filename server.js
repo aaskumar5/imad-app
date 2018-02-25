286b991f537ca2ace5ca855b69ff3f7c1cf09543
Ashish Kumar sahu <32214433+aaskumar5@users.noreply.github.com>
2018-02-25 22:47:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var congif = {
+    user: 'ashishkumarsahu2016',
+    database: 'ashishkumarsahu2016',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -77,6 +86,18 @@ function createTemplate(data){
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(congif);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM TEST',function(err,result){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+    
 });
 
 var counter = 0;
